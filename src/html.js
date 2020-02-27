347eaea567c6826eebb4f02b443e4ebcf73831ec
haritonasty <a.kharitonovaspb@gmail.com>
2020-02-27 17:53:17 +0300

    upd gtag

--- src/html.js
+++ src/html.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const intercomScript = (
   <script
     dangerouslySetInnerHTML={{
-      __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/cyjjko9u';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`,
+      __html: `(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/cyjjko9u';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()`
     }}
   />
 )
@@ -12,16 +12,16 @@ const intercomScript = (
 const gtagScript = (
   <script
     dangerouslySetInnerHTML={{
-      __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+      __html: `  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-gtag('config', 'UA-100571693-8');`,
+  gtag('config', 'UA-100571693-1');`
     }}
   />
 )
 
-export default function HTML(props) {
+export default function HTML (props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
@@ -35,7 +35,7 @@ export default function HTML(props) {
         {intercomScript}
         <script
           async
-          src='https://www.googletagmanager.com/gtag/js?id=UA-100571693-8'
+          src='https://www.googletagmanager.com/gtag/js?id=UA-100571693-1%22%3E'
         />
         {gtagScript}
       </head>
@@ -61,5 +61,5 @@ HTML.propTypes = {
   bodyAttributes: PropTypes.object,
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
-  postBodyComponents: PropTypes.array,
+  postBodyComponents: PropTypes.array
 }
