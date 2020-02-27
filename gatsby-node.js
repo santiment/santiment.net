f528901c5230e5c1cdae691978551601db78159d
haritonasty <a.kharitonovaspb@gmail.com>
2020-02-27 17:11:59 +0300

    fix isPermanent

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,15 +8,15 @@
 
 exports.createPages = ({ graphql, actions }) => {
   const { createRedirect } = actions
-  createRedirect({ fromPath: '/sansheets', toPath: 'https://sheets.santiment.net/', isPermanent: true })
-  createRedirect({ fromPath: '/sandata', toPath: 'https://data.santiment.net/', isPermanent: true })
-  createRedirect({ fromPath: '/about-santiment/how-to-buy-san/', toPath: 'https://academy.santiment.net/san-tokens/how-to-buy-san/', isPermanent: true })
+  createRedirect({ fromPath: '/sansheets', toPath: 'https://sheets.santiment.net/' })
+  createRedirect({ fromPath: '/sandata', toPath: 'https://data.santiment.net/' })
+  createRedirect({ fromPath: '/about-santiment/how-to-buy-san/', toPath: 'https://academy.santiment.net/san-tokens/how-to-buy-san/' })
 
-  createRedirect({ fromPath: '/about-santiment/team/', toPath: '/team', isPermanent: true })
-  createRedirect({ fromPath: '/about-santiment/transparency/', toPath: '/', isPermanent: true })
-  createRedirect({ fromPath: '/about-santiment/jobs/', toPath: '/jobs', isPermanent: true })
-  createRedirect({ fromPath: '/about-santiment', toPath: '/about', isPermanent: true })
-  createRedirect({ fromPath: '/about-santiment/contact/', toPath: '/contact', isPermanent: true })
-  createRedirect({ fromPath: '/discord', toPath: 'https://discordapp.com/invite/MPH2uP5', isPermanent: true })
-  createRedirect({ fromPath: '/terms-conditions/', toPath: '/terms', isPermanent: true })
+  createRedirect({ fromPath: '/about-santiment/team/', toPath: '/team' })
+  createRedirect({ fromPath: '/about-santiment/transparency/', toPath: '/' })
+  createRedirect({ fromPath: '/about-santiment/jobs/', toPath: '/jobs' })
+  createRedirect({ fromPath: '/about-santiment', toPath: '/about' })
+  createRedirect({ fromPath: '/about-santiment/contact/', toPath: '/contact' })
+  createRedirect({ fromPath: '/discord', toPath: 'https://discordapp.com/invite/MPH2uP5' })
+  createRedirect({ fromPath: '/terms-conditions/', toPath: '/terms' })
 }
