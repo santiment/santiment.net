6931274771e7d05437b4690fe76a3c7e4d45ea75
haritonasty <a.kharitonovaspb@gmail.com>
2020-02-27 12:27:03 +0300

    404 small fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Subheading from '../components/Heading/Subheading'
+import styles from './404.module.scss'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.wrapper}>
+    <Subheading className={styles.title}>Not found</Subheading>
+    <p className={styles.desc}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
   </Layout>
 )
 
