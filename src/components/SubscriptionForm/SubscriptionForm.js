4c8489f1fd0eca9c7d6e77f04fa8441f9c50cc2f
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-05 15:53:15 +0300

    add error state for form

--- src/components/SubscriptionForm/SubscriptionForm.js
+++ src/components/SubscriptionForm/SubscriptionForm.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cx from 'classnames'
 import Button from '@santiment-network/ui/Button'
 import Input from '@santiment-network/ui/Input'
+import Label from '@santiment-network/ui/Label'
 import Icon from '@santiment-network/ui/Icon'
+import debounce from 'lodash.debounce'
 import styles from './SubscriptionForm.module.scss'
 
 const SubscriptionForm = ({
@@ -12,40 +14,68 @@ const SubscriptionForm = ({
   send,
   add,
   classes = {}
-}) => (
-  <form
-    className={cx(styles.form, classes.form)}
-    onSubmit={e => {
-      e.preventDefault()
-      send({
-        variables: {
-          email: e.currentTarget.email.value
-        }
-      }).then(() => {
-        add({
-          type: 'success',
-          title: 'Verification email was sent to the provided email!'
+}) => {
+  const [waiting, setWaiting] = useState(false)
+  const [error, setError] = useState(false)
+  const [email, setEmail] = useState()
+
+  const onEmailChange = ({ currentTarget: { value } }) => {
+    setEmail(value)
+    checkEmail(value)
+  }
+
+  const checkEmail = debounce(email => {
+    let error
+    if (!email) {
+      error = 'Email is required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      error = 'Invalid email address'
+    }
+
+    setError(error)
+  })
+
+  return (
+    <form
+      className={cx(styles.form, classes.form)}
+      onSubmit={e => {
+        e.preventDefault()
+        send({
+          variables: { email }
+        }).then(() => {
+          add({
+            type: 'success',
+            title: 'Verification email was sent to the provided email!'
+          })
         })
-      })
-    }}
-  >
-    <Input
-      className={cx(styles.input, classes.input)}
-      type='email'
-      required
-      placeholder={placeholder}
-      name='email'
-    />
-    <Button
-      className={cx(styles.btn, classes.button)}
-      variant='fill'
-      accent='positive'
-      isLoading={loading}
+      }}
     >
-      {cta}
-      <Icon type='pointer-right' className={styles.arrow} />
-    </Button>
-  </form>
-)
+      <Input
+        className={cx(styles.input, classes.input)}
+        type='email'
+        placeholder={placeholder}
+        name='email'
+        disabled={waiting}
+        onChange={onEmailChange}
+        isError={error}
+      />
+      {error && email && (
+        <Label accent='persimmon' className={styles.formError}>
+          {error}
+        </Label>
+      )}
+      <Button
+        className={cx(styles.btn, classes.button)}
+        variant='fill'
+        accent='positive'
+        disabled={error && email}
+        isLoading={waiting || loading}
+      >
+        {cta}
+        <Icon type='pointer-right' className={styles.arrow} />
+      </Button>
+    </form>
+  )
+}
 
 export default SubscriptionForm
