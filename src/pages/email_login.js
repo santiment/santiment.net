cf9dff4ad4df859f5759478b105b8da7e1707947
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-06 17:13:38 +0300

    undefined checking

--- src/pages/email_login.js
+++ src/pages/email_login.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const EmailPage = ({ location: { search } }) => {
-  if (window) {
+  if (window !== 'undefined') {
     window.location.href = `https://app.santiment.net/email_login${search}`
   }
   return null
