0e5ca718d159663f2aef2eb1a681b607cfc0cd77
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-06 17:10:25 +0300

    check if window

--- src/pages/email_login.js
+++ src/pages/email_login.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Layout from '../components/layout'
 
 const EmailPage = ({ location: { search } }) => {
-	window.location.href=`https://app.santiment.net/email_login${search}`
-	return null
+  if (window) {
+    window.location.href = `https://app.santiment.net/email_login${search}`
+  }
+  return null
 }
 
 export default EmailPage
