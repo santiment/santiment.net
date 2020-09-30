5af9b433bc0fdc95cf92d62e0bb7556df72d4015
Nastya Kharitonova <a.kharitonovaspb@gmail.com>
2020-09-30 17:13:13 +0300

    change discord link

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,7 @@ exports.createPages = ({ graphql, actions }) => {
   createRedirect({ fromPath: '/about-santiment/jobs/', toPath: '/jobs' })
   createRedirect({ fromPath: '/about-santiment', toPath: '/about' })
   createRedirect({ fromPath: '/about-santiment/contact/', toPath: '/contact' })
-  createRedirect({ fromPath: '/discord', toPath: 'https://discordapp.com/invite/MPH2uP5' })
+  createRedirect({ fromPath: '/discord', toPath: 'https://discord.gg/KtFrY4P' })
   createRedirect({ fromPath: '/terms-conditions/', toPath: '/terms' })
 
 
