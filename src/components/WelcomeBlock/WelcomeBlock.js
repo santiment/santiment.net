87b6d591e951040ddf12a8f3817ce7aa490ac40f
haritonasty <a.kharitonovaspb@gmail.com>
2021-04-26 17:31:44 +0300

    intro layout

--- src/components/WelcomeBlock/WelcomeBlock.js
+++ src/components/WelcomeBlock/WelcomeBlock.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import cx from 'classnames'
 import { Mutation } from 'react-apollo'
-import Subheading from '../Heading/Subheading'
 import { EMAIL_LOGIN_MUTATION } from '../../gql/user'
 import { NotificationsContext } from '../Notifications/Notifications'
 import SubscriptionForm from '../SubscriptionForm/SubscriptionForm'
+import topSpot from './top-spot.svg'
+import bottomSpot from './bottom-spot.svg'
+import Eth from './eth.svg'
+import Bitcoin from './bitcoin.svg'
+import Zcash from './zcash.svg'
+import Ripple from './ripple.svg'
+import Litecoin from './litecoin.svg'
+import Dash from './dash.svg'
 import styles from './WelcomeBlock.module.scss'
 
 const WelcomeBlock = () => (
   <section className={styles.wrapper}>
-    <Subheading className={styles.title}>
-      Tools to help you analyze the crypto market and find data-driven investing
-      opportunities
-    </Subheading>
-    <div className={styles.description}>
-      Santiment is a behavior analytics platform for cryptocurrencies, sourcing
-      on-chain, social and development information on 900+ coins.
-    </div>
+      <img src={topSpot} className={styles.topSpot} alt="" />
+      <img src={bottomSpot} className={styles.bottomSpot} alt="" />
+      <img src={Dash} className={cx(styles.dash, styles.coin)} alt="dash" />
+      <img src={Litecoin} className={cx(styles.litecoin, styles.coin)} alt="litecoin" />
+      <img src={Ripple} className={cx(styles.ripple, styles.coin)} alt="ripple" />
+      <img src={Eth} className={cx(styles.eth, styles.coin)} alt="eth" />
+      <img src={Bitcoin} className={cx(styles.bitcoin, styles.coin)} alt="bitcoin" />
+      <img src={Zcash} className={cx(styles.zcash, styles.coin)} alt="zcash" />
+    <h1 className={styles.title}>
+        On-chain, social & fundamental insights on 1500+ cryptocurrencies
+    </h1>
     <NotificationsContext.Consumer>
       {({ add }) => (
         <Mutation mutation={EMAIL_LOGIN_MUTATION}>
