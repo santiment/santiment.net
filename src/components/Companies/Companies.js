2529ca1b15d106f2cdcd4c78eb0ecc4378abcdaa
haritonasty <a.kharitonovaspb@gmail.com>
2021-04-29 20:41:59 +0300

    upd companies

--- src/components/Companies/Companies.js
+++ src/components/Companies/Companies.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
-import BloomberImg from './bloomberg.svg'
-import ForbesImg from './forbes.svg'
-import CoindeskImg from './coindesk.svg'
-import CoinTelegraphImg from './cointelegraph.svg'
-import CryptoBrImg from './crypto-briefing.svg'
-import BitcoinImg from './bitcoin.svg'
+import BloomberImg from './bloomberg.png'
+import ForbesImg from './forbes.png'
+import CoindeskImg from './coindesk.png'
+import CoinTelegraphImg from './cointelegraph.png'
+import CryptoBrImg from './cb.png'
+import BitcoinImg from './bitcoin.png'
 import styles from './Companies.module.scss'
 
 const Companies = () => {
   return (
     <div className={styles.container}>
       <Heading className={styles.header}>
-        Big companies that referenced our data
+          As referenced in major publications
       </Heading>
       <div className={styles.list}>
-        <img alt='bloomberg' src={BloomberImg} />
-        <img alt='forbes' src={ForbesImg} />
-        <img alt='coindesk' src={CoindeskImg} />
-        <img alt='cointelegraph' src={CoinTelegraphImg} />
-        <img alt='cryptobriefing' src={CryptoBrImg} />
-        <img alt='bitcoin' src={BitcoinImg} />
+        <img alt='bloomberg' src={BloomberImg} width="217"  />
+        <img alt='forbes' src={ForbesImg} width="163" />
+        <img alt='coindesk' src={CoindeskImg} width="205"  />
+        <img alt='cointelegraph' src={CoinTelegraphImg} width="258"  />
+        <img alt='cryptobriefing' src={CryptoBrImg} width="148"  />
+        <img alt='bitcoin' src={BitcoinImg} width="255"  />
       </div>
     </div>
   )
