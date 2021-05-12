85ac0bd030a587d6beae5903d6c22cc1dbd30517
haritonasty <a.kharitonovaspb@gmail.com>
2021-05-12 14:15:25 +0300

    lazy load twitter feedbacks

--- src/components/TwitterFeedbacks/TwitterFeedbacks.js
+++ src/components/TwitterFeedbacks/TwitterFeedbacks.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Slider from 'react-slick'
 import Icon from '@santiment-network/ui/Icon'
 import DesktopTweets from './DesktopTweets/DesktopTweets'
 import { TweetCard, TweetsParsed } from './Tweets'
+import Testimonials from "../Testimonials";
 import styles from './TwitterFeedbacks.module.scss'
 
 export const SLIDER_SETTINGS = {
@@ -11,6 +12,10 @@ export const SLIDER_SETTINGS = {
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false
+}
+
+const OPTIONS = {
+  threshold: 0
 }
 
 export const useSlider = () => {
@@ -30,34 +35,55 @@ const TwitterBg = ({ className }) => (
 )
 
 const TwitterFeedbacks = () => {
+  const containerRef = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  const cbFunc = entries => {
+    const [entry] = entries
+    if (!isVisible && entry.isIntersecting) {
+      setIsVisible(true)
+      const script = document.createElement('script')
+      script.src = 'https://platform.twitter.com/widgets.js'
+      script.async = true
+      document.body.appendChild(script)
+    }
+  }
+
   useEffect(() => {
-    const script = document.createElement('script')
-    script.src = 'https://platform.twitter.com/widgets.js'
-    script.async = true
-    document.body.appendChild(script)
-  }, [])
+    const observer = new IntersectionObserver(cbFunc, OPTIONS)
+    if (containerRef.current) {
+      observer.observe(containerRef.current)
+    }
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current)
+    }
+  }, [containerRef, OPTIONS])
 
   const { slider } = useSlider()
 
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <TwitterBg className={styles.headerBg} />
-        <div className={styles.title}>
-          <TwitterBg className={styles.twitterBlue} />
-          More reviews from Twitter
+    <div ref={containerRef}>
+      <Testimonials />
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <TwitterBg className={styles.headerBg} />
+          <div className={styles.title}>
+            <TwitterBg className={styles.twitterBlue} />
+            More reviews from Twitter
+          </div>
         </div>
-      </div>
-      <div className={styles.list}>
-        <DesktopTweets />
-      </div>
-      <div className={styles.slider}>
-        <Slider {...SLIDER_SETTINGS} ref={slider}>
-          {TweetsParsed.map((item, index) => (
-            <TweetCard item={item} key={index} />
-          ))}
-        </Slider>
-      </div>
+            <div className={styles.list}>
+              <DesktopTweets />
+            </div>
+            <div className={styles.slider}>
+              <Slider {...SLIDER_SETTINGS} ref={slider}>
+                {TweetsParsed.map((item, index) => (
+                  <TweetCard item={item} key={index} />
+                ))}
+              </Slider>
+            </div>
+    </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,13 @@ import React, { useEffect } from 'react'
 import { injectIntl } from 'gatsby-plugin-intl'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Delayed from "../components/Delayed"
 import Cases from '../components/Cases/Cases'
-import Testimonails from '../components/Testimonials'
 import Features from '../components/Features/Features'
 import Products from '../components/Products/Products'
 import Companies from '../components/Companies/Companies'
 import GetStarted from '../components/GetStarted/GetStarted'
 import WelcomeBlock from '../components/WelcomeBlock/WelcomeBlock'
-import TwitterFeedbacks from '../components/TwitterFeedbacks/TwitterFeedbacks'
+import Feedbacks from '../components/TwitterFeedbacks/TwitterFeedbacks'
 import styles from './index.module.scss'
 
 const IndexPage = ({ intl, location }) => {
@@ -28,11 +26,8 @@ const IndexPage = ({ intl, location }) => {
             <Features />
             <Products />
             <Companies />
-            <Testimonails />
-            <Delayed waitBeforeShow="3000">
-            <TwitterFeedbacks />
+            <Feedbacks />
             <GetStarted />
-            </Delayed>
         </Layout>
     )
 }
