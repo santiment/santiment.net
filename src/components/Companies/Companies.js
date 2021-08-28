16bf7e68aea4d6e36d2e8e3bfc67186d99553959
Anastasya Kharitonova <haritonasty@pm.me>
2021-08-28 14:14:18 +0300

    new company

--- src/components/Companies/Companies.js
+++ src/components/Companies/Companies.js
@@ -6,6 +6,7 @@ import CoindeskImg from './coindesk.png'
 import CoinTelegraphImg from './cointelegraph.png'
 import CryptoBrImg from './cb.png'
 import BitcoinImg from './bitcoin.png'
+import Newyorker from './new-yorker.png'
 import styles from './Companies.module.scss'
 
 const Companies = () => {
@@ -15,13 +16,18 @@ const Companies = () => {
           As referenced in major publications
       </Heading>
       <div className={styles.list}>
-        <img alt='bloomberg' src={BloomberImg} width="217"  />
-        <img alt='forbes' src={ForbesImg} width="163" />
-        <img alt='coindesk' src={CoindeskImg} width="205"  />
+        <div className={styles.row}>
+        <img alt='bloomberg' src={BloomberImg} width="260"  />
+        <img alt='forbes' src={ForbesImg} width="176" />
+        <img alt='coindesk' src={CoindeskImg} width="226"  />
+      </div>
+      <div className={styles.row}>
         <img alt='cointelegraph' src={CoinTelegraphImg} width="258"  />
-        <img alt='cryptobriefing' src={CryptoBrImg} width="148"  />
+        <img alt='cryptobriefing' src={CryptoBrImg} width="163"  />
+        <img alt='newyorker' src={Newyorker} width="195"  />
         <img alt='bitcoin' src={BitcoinImg} width="255"  />
       </div>
+    </div>
     </div>
   )
 }
