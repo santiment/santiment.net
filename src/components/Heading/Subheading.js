efc4044f6170d2eace36cecd5e5725b783178e35
DmitriVanGuard <dimavanguard@gmail.com>
2023-12-14 21:05:44 +0100

    about page seo update

--- src/components/Heading/Subheading.js
+++ src/components/Heading/Subheading.js
@@ -2,8 +2,8 @@ import React from 'react'
 import cx from 'classnames'
 import styles from './Subheading.module.scss'
 
-const Subheading = ({ className, children }) => (
-  <h4 className={cx(styles.text, className)}>{children}</h4>
+const Subheading = ({ className, children, El = 'h4' }) => (
+  <El className={cx(styles.text, className)}>{children}</El>
 )
 
 export default Subheading
--- src/pages/about.js
+++ src/pages/about.js
@@ -14,38 +14,33 @@ import styles from './about.module.scss'
 const AboutPage = ({ intl }) => (
   <Layout>
     <SEO
-      title='Signals and Context for the crypto ecosystem'
-      description='From the beginning, our team has been guided by a single mission - to eliminate information asymmetry in the crypto ecosystem...'
+      title="Crypto Analytics Platform: Blockchain Data to Guide Investments"
+      description="As a crypto analytics platform, our team has been guided by a single mission - to eliminate information asymmetry in the crypto ecosystem by using blockchain data."
     />
     <div className={styles.wrapper}>
-      <Subheading className={styles.title}>
-        Signals and Context for the crypto ecosystem
+      <Subheading El="h1" className={styles.title}>
+        Crypto analytics platform for Signals and Context
       </Subheading>
       <div className={styles.desc}>
         <div>
-          From the beginning, our team has been guided by a single mission - to
-          eliminate <strong>information asymmetry</strong> in the crypto
-          ecosystem.
+          From the beginning, our team has been guided by a single mission - to eliminate{' '}
+          <strong>information asymmetry</strong> in the crypto ecosystem.
         </div>
         <div>
-          Blockchain technology is changing the world, but there’s still a
-          concerning lack of credible, actionable information on the crypto
-          industry and its major stakeholders.
+          Blockchain technology is changing the world, but there’s still a concerning lack of
+          credible, actionable information on the crypto industry and its major stakeholders.
         </div>
       </div>
       <h3 className={styles.heading}>Everyone is guessing</h3>
       <div className={styles.text}>
-        Our mission is to block out the noise and provide data-driven insights
-        on cryptocurrency networks, blockchain-based projects, crowd sentiment
-        and market behavior.
+        Our mission is to block out the noise and provide data-driven insights on cryptocurrency
+        networks, blockchain-based projects, crowd sentiment and market behavior.
       </div>
       <Heading>Our team</Heading>
       <div className={styles.team}>
-        <img src={team} alt='team' />
+        <img src={team} alt="team" />
       </div>
-      <div className={styles.info}>
-        34 experts | 12 countries | 2016 founded | 57k ETH raised
-      </div>
+      <div className={styles.info}>34 experts | 12 countries | 2016 founded | 57k ETH raised</div>
       <Steps />
       <AboutSAN />
       <Products className={styles.products} />
