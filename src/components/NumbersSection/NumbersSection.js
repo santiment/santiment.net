f4d037b3109ab5971cb29ffacf7a8777b8911db9
Vlad Keliy <vekkele@gmail.com>
2023-12-26 12:24:50 +0300

    fix(home) Fix numbers section layout

--- src/components/NumbersSection/NumbersSection.js
+++ src/components/NumbersSection/NumbersSection.js
@@ -6,8 +6,8 @@ import MainNumbers from './MainNumbers'
 import Numbers from './Numbers'
 
 const NumbersSection = () => (
-  <section className={cx(styles.wrapper, 'row hv-center c-white')}>
-    <div className={cx(styles.content, 'column')}>
+  <div className={cx(styles.wrapper, 'row hv-center c-white')}>
+    <section className={cx(styles.content, 'column')}>
       <div className={cx(styles.main, 'row')}>
         <Leading />
 
@@ -15,8 +15,8 @@ const NumbersSection = () => (
       </div>
 
       <Numbers />
-    </div>
-  </section>
+    </section>
+  </div>
 )
 
 export default NumbersSection
