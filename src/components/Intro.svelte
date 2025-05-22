521ae6e66b156c17330dbe01b745c5ebc26b403b
Serafim <serafim.g@santiment.net>
2025-05-22 20:02:57 +0400

    feat: intro section

---
+++ src/components/Intro.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+</script>
+
+<section class="py-40">
+  <div class="container flex gap-[8.75rem] lg:flex-col-reverse lg:px-6 md:gap-[2.875rem] md:px-4">
+    <img
+      src="/img.png"
+      class="-ml-[5.625rem] -mr-[5.625rem] max-w-[43.75rem] md:m-0 md:max-w-full"
+      alt=""
+    />
+    <div class="max-w-[30rem]">
+      <p class="mb-1 text-base font-semibold text-fiord">All-in-one platform</p>
+      <h2 class="mb-6 text-4xl font-semibold text-rhino">Crypto intelligence tools</h2>
+      <p class="mb-2.5 text-lg text-rhino">
+        We bring together everything that you can use to make your next action. Datasets,{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://app.santiment.net/screener/new"
+          class="link-pointer"
+        >
+          screeners
+        </a>
+        ,{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://app.santiment.net/watchlists"
+          class="link-pointer"
+        >
+          watchlists
+        </a>
+        ,{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://app.santiment.net/alerts"
+          class="link-pointer"
+        >
+          alerts
+        </a>
+        , flexible{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://app.santiment.net/charts"
+          class="link-pointer"
+        >
+          chart layouts
+        </a>
+        ,{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://app.santiment.net/dashboards"
+          class="link-pointer"
+        >
+          dashboards
+        </a>
+        ,{' '}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://insights.santiment.net/"
+          class="link-pointer"
+        >
+          insights
+        </a>{' '}
+        from the community and the San analytics team.
+      </p>
+      <p class="mb-[5.625rem] text-lg text-rhino">
+        Since 2016, Santiment has produced a platform to research crypto data intelligence tools for
+        hedge fund managers, retail investors, crypto project owners, and NFT creators.
+      </p>
+      <div class="flex gap-12 center">
+        <div class="relative column center">
+          <img
+            src="/back-green.svg"
+            class="absolute -left-[12px] -top-[6px] -z-[1] h-[42px] w-[47px] sm:-left-[20px] sm:-top-[10px]"
+            alt=""
+          />
+          <h4 class="text-xl text-rhino">Financal</h4>
+          <p class="text-base text-fiord">datasets</p>
+        </div>
+        <div class="relative column center">
+          <img
+            src="/back-yellow.svg"
+            class="absolute -left-[10px] -top-[10px] -z-[1] h-[44px] w-[50px] sm:-left-[20px] sm:-top-[10px]"
+            alt=""
+          />
+          <h4 class="text-xl text-rhino">On-chain</h4>
+          <p class="text-base text-fiord">datasets</p>
+        </div>
+        <div class="relative column center">
+          <img
+            src="/back-grey.svg"
+            class="absolute -left-[16px] -top-[10px] -z-[1] h-[44px] w-[56px] sm:-left-[20px] sm:-top-[10px]"
+            alt=""
+          />
+          <h4 class="text-xl text-rhino">Social</h4>
+          <p class="text-base text-fiord">datasets</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
