ce3b284f2596e143a701cc1fe2033ccc60115ae5
Serafim <serafim@Serafims-MacBook-Pro.local>
2025-06-03 12:32:57 +0400

    fix: nav border

--- src/components/Nav/Nav.svelte
+++ src/components/Nav/Nav.svelte
@@ -75,9 +75,14 @@
         <Svg id="close" class="fill-waterloo" />
       </button>
 
-      <div class="mb-4">
+      <div class="mb-6 border-b border-b-porcelain pb-3">
         <Products
-          class="w-full flex-col p-0 [&>div>section>a>div:has(svg)]:h-8 [&>div>section>a>div:has(svg)]:w-8 [&>div>section>a>div:has(svg)]:min-w-8 [&>div>section]:pb-3 first:[&>div>section]:border-b first:[&>div>section]:border-b-porcelain [&>div]:border-b [&>div]:border-b-porcelain [&_a]:items-center [&_a]:px-4 [&_a]:py-2 [&_h2]:font-medium [&_h3]:mb-2 [&_p]:hidden"
+          class={cn(
+            'w-full flex-col p-0 [&>div]:border-b [&>div]:border-b-porcelain',
+            '[&>div>section>a>div:has(svg)]:h-8 [&>div>section>a>div:has(svg)]:w-8 [&>div>section>a>div:has(svg)]:min-w-8',
+            '[&>div>section]:pb-3 first:[&>div>section]:border-b first:[&>div>section]:border-b-porcelain',
+            '[&_a]:items-center [&_a]:px-4 [&_a]:py-2 [&_h2]:font-medium [&_h3]:mb-2 [&_p]:hidden',
+          )}
         />
       </div>
 
