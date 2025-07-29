59c6eee48d9ff24b3e48d094a61a8b37cc20b72b
Serafim <serafim@Serafims-MacBook-Pro.local>
2025-07-29 14:14:09 +0400

    chore: wrapped astro config into async IIFE

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,27 @@ import tailwind from '@astrojs/tailwind'
 import { defineConfig, mergeConfig } from 'astro/config'
 import { createAstroConfig } from 'san-webkit-next/vite.config.js'
 
-const viteBase = await createAstroConfig()
+export default (async () => {
+  const viteBase = await createAstroConfig()
 
-const viteConfig = mergeConfig(viteBase, {
-  define: {
-    __SVELTEKIT_APP_VERSION_POLL_INTERVAL__: '0',
-  },
-})
+  const viteConfig = mergeConfig(viteBase, {
+    define: {
+      __SVELTEKIT_APP_VERSION_POLL_INTERVAL__: '0',
+      'process.env.MEDIA_PATH': JSON.stringify('/webkit'),
+      'process.env.ICONS_PATH': JSON.stringify(process.env.MEDIA_PATH + '/icons'),
+    },
+  })
 
-export default defineConfig({
-  integrations: [svelte(), tailwind()],
-  vite: viteConfig,
-  publicDir: './static',
-  outDir: './public',
-  server: {
-    port: 3000,
-  },
-})
+  return defineConfig({
+    integrations: [svelte(), tailwind()],
+    vite: viteConfig,
+    ssr: {
+      noExternal: ['san-webkit-next', 'san-webkit'],
+    },
+    publicDir: './static',
+    outDir: './public',
+    server: {
+      port: 3000,
+    },
+  })
+})()
