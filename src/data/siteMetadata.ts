e4359221978b5a3ef0948aae4989250e82a14737
Serafim <serafim@Serafims-MacBook-Pro.local>
2025-10-01 10:59:41 -0300

    chore: copied SEO practices from legacy site

--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -1,5 +1,5 @@
 export const siteMetadata = {
-  title: `Crypto Behavior Analytics Platform`,
+  title: `Crypto Research, Data, Tools - Explore Behavioral Analytics`,
   description: `We provide tools for crypto research to analyze markets and find data-driven opportunities to optimize your investing. Dive in and explore our on-chain, social and development data.`,
   author: `Santiment`,
   siteUrl: 'https://santiment.net',
