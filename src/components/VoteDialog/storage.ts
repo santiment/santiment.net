4e467e4434b6e5f2129c56f5f3917a123e4eed7e
Vlad Keliy <vekkele@gmail.com>
2026-04-07 13:23:13 +0300

    chore: Remove VoteDialog

--- src/components/VoteDialog/storage.ts
+++
@@ -1,9 +0,0 @@
-import { saveBoolean, getSavedBoolean } from 'san-webkit-next/utils/localStorage'
-
-const KEY = 'HEDGEWEEK_VOTE_DIALOG_CLOSED_2026'
-
-export const saveDialogClosed = () => {
-  saveBoolean(KEY, true)
-}
-
-export const getSavedDialogClosed = () => getSavedBoolean(KEY) ?? false
